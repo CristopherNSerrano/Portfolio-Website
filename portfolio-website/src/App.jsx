d01f658591139362699a7706f9873fee66f4f79e
CristopherNSerrano <cristopherserranoutsa@gmail.com>
2025-02-13 13:55:31 -0600

    Created React/Vite Project

---
+++ portfolio-website/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>
+      <div className='container bg-red-500 mx-auto'>
+        <h1 className='text-8xl text-sky-100'>Hello World!!</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
